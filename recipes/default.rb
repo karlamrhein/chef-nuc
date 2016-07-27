293b65889ad70a93803b34b0ebb5374876bb45ec
Karl Amrhein <karlamrhein@gmail.com>
2016-07-27 00:50:10 -0600

    install basic stuff on nuc server

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,35 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package ['screen', 'xhost', 'xauth', 'epel-release', 'git']
+
+# Install chefdk
+remote_file "#{Chef::Config[:file_cache_path]}/chefdk-0.16.28-1.el7.x86_64.rpm" do
+  source'https://packages.chef.io/stable/el/7/chefdk-0.16.28-1.el7.x86_64.rpm'
+  action :create
+end
+yum_package 'chefdk-0.16.28-1.el7.x86_64.rpm' do
+  source "#{Chef::Config[:file_cache_path]}/chefdk-0.16.28-1.el7.x86_64.rpm"
+  action :install
+end
+
+# Install vagrant
+remote_file "#{Chef::Config[:file_cache_path]}/vagrant_1.8.5_x86_64.rpm" do
+  source'https://releases.hashicorp.com/vagrant/1.8.5/vagrant_1.8.5_x86_64.rpm'
+  action :create
+end
+yum_package "vagrant_1.8.5_x86_64.rpm" do
+  source "#{Chef::Config[:file_cache_path]}/vagrant_1.8.5_x86_64.rpm"
+  action :install
+end
+
+# Install virtualbox
+remote_file "#{Chef::Config[:file_cache_path]}/VirtualBox-5.1-5.1.2_108956_el7-1.x86_64.rpm" do
+  source "http://download.virtualbox.org/virtualbox/5.1.2/VirtualBox-5.1-5.1.2_108956_el7-1.x86_64.rpm"
+  action :create
+end
+yum_package "VirtualBox-5.1-5.1.2_108956_el7-1.x86_64.rpm" do
+  source "#{Chef::Config[:file_cache_path]}/VirtualBox-5.1-5.1.2_108956_el7-1.x86_64.rpm"
+  action :install
+end
